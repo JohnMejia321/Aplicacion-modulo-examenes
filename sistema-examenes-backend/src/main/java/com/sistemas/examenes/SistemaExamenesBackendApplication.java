6cbb01aeb2bf818d024ef13ae954f7f3077447cb
JohnMejia <john.mejias@udea.edu.co>
2023-04-20 00:02:40 -0500

    se agrega metodo post en controller user

--- sistema-examenes-backend/src/main/java/com/sistemas/examenes/SistemaExamenesBackendApplication.java
+++ sistema-examenes-backend/src/main/java/com/sistemas/examenes/SistemaExamenesBackendApplication.java
@@ -47,7 +47,7 @@ public class SistemaExamenesBackendApplication implements CommandLineRunner {
 
 			*/
 
-		
+
 
 	}
 }
--- sistema-examenes-backend/src/main/java/com/sistemas/examenes/controladores/UsuarioController.java
+++ sistema-examenes-backend/src/main/java/com/sistemas/examenes/controladores/UsuarioController.java
@@ -1,11 +1,15 @@
 package com.sistemas.examenes.controladores;
 
+import com.sistemas.examenes.entidades.Rol;
 import com.sistemas.examenes.entidades.Usuario;
+import com.sistemas.examenes.entidades.UsuarioRol;
 import com.sistemas.examenes.servicios.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/api/usuarios")
@@ -14,6 +18,25 @@ public class UsuarioController {
 
     @Autowired
     UsuarioService usuarioService;
+
+
+    @PostMapping
+    public Usuario guardarUsuario(@RequestBody Usuario usuario) throws Exception{
+        usuario.setPerfil("default.png");
+        Set<UsuarioRol> usuarioRoles = new HashSet<>();
+
+        Rol rol = new Rol();
+        rol.setRolId(2L);
+        rol.setRolNombre("NORMAL");
+
+        UsuarioRol usuarioRol = new UsuarioRol();
+        usuarioRol.setUsuario(usuario);
+        usuarioRol.setRol(rol);
+
+        usuarioRoles.add(usuarioRol);
+        return usuarioService.guardarUsuario(usuario,usuarioRoles);
+    }
+
 
     @GetMapping("/{username}")
     public Usuario obtenerUsuario(@PathVariable("username") String username){
