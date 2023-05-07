1b515ee3318710db7b012756cce703384e108a3e
JohnMejia <john.mejias@udea.edu.co>
2023-05-07 18:28:51 -0500

    se edita navbar para que renderice el estado de login o logout

--- sistema-examenes-frontend/src/app/components/navbar/navbar.component.ts
+++ sistema-examenes-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from 'src/app/services/login/login.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   isLoggedIn = false;
+  user:any = null;
 
-  constructor() { }
+  constructor(public login:LoginService) { }
 
   ngOnInit(): void {
+    this.isLoggedIn = this.login.isLoggedIn();
+    this.user = this.login.getUser();
+    this.login.loginStatusSubjec.asObservable().subscribe(
+      data => {
+        this.isLoggedIn = this.login.isLoggedIn();
+        this.user = this.login.getUser();
+      }
+    )
+  }
+
+  public logout(){
+    this.login.logout();
+    window.location.reload();
   }
 
 }
