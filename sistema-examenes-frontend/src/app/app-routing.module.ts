3f557a1bc1d450d5dbb8b4cbccb95a9a81f3cddb
JohnMejia <john.mejias@udea.edu.co>
2023-05-07 17:55:46 -0500

    se protege ruta de admin

--- sistema-examenes-frontend/src/app/app-routing.module.ts
+++ sistema-examenes-frontend/src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { UserDashboardComponent } from './pages/user/user-dashboard/user-dashboard.component';
-//import { AdminGuard } from './services/admin.guard';
-//import { NormalGuard } from './services/normal.guard';
+import { AdminGuard } from './services/admin.guard';
+import { NormalGuard } from './services/normal.guard';
 
 const routes: Routes = [
   {
@@ -28,13 +28,13 @@ const routes: Routes = [
     path:'admin',
     component:DashboardComponent,
     pathMatch:'full',
-   // canActivate:[AdminGuard]
+   canActivate:[AdminGuard]
   },
   {
     path:'user-dashboard',
     component:UserDashboardComponent,
     pathMatch:'full',
-    //canActivate:[NormalGuard]
+    canActivate:[NormalGuard]
   }
 ];
 
